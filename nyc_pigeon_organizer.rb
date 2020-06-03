554f38353e27bef0ba02cf7b1129669f92e882ad
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-03 19:41:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  data.
+  new_hash = {}
+  
+  data.each do |a_name, a|
+    a.each do |a_value, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][a_name] ||= []
+        new_hash[name][a_name].push(a_value.to_s)
+      end
+    end
+  end
+  new_hash
 end
